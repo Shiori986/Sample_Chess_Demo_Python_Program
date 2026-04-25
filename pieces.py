667d7a9212f92de99bef54b0a3fd046e7347fcb6
Christopher Higgins <cqhiggin@utica.edu>
2026-04-24 20:06:43 -0400

    Add files via upload
    This program is essental because it provides the program with the ascii chess peices that the program needs and it tells the program what each chess peice is and basically how they are supposed to move on the board, so that the program knows what chess peices line up to which and what their functions are. This part was one of the most painful challenges for me because when I fixed one chess peice another peice would sometimes break. Eventually, I was able to get it to a stable part that was satifactory.

---
+++ pieces.py
@@ -0,0 +1,24 @@
+# ================================
+#    ASCII / UNICODE PIECES WITH COLOR
+# ================================
+
+# ANSI foreground colors
+WHITE_FG = "\033[97m"   # bright white
+BLACK_FG = "\033[30m"   # black
+RESET    = "\033[0m"
+
+# WHITE PIECES
+WHITE_PAWN   = WHITE_FG + "♙" + RESET
+WHITE_ROOK   = WHITE_FG + "♖" + RESET
+WHITE_KNIGHT = WHITE_FG + "♘" + RESET
+WHITE_BISHOP = WHITE_FG + "♗" + RESET
+WHITE_QUEEN  = WHITE_FG + "♕" + RESET
+WHITE_KING   = WHITE_FG + "♔" + RESET
+
+# BLACK PIECES
+BLACK_PAWN   = BLACK_FG + "♟" + RESET
+BLACK_ROOK   = BLACK_FG + "♜" + RESET
+BLACK_KNIGHT = BLACK_FG + "♞" + RESET
+BLACK_BISHOP = BLACK_FG + "♝" + RESET
+BLACK_QUEEN  = BLACK_FG + "♛" + RESET
+BLACK_KING   = BLACK_FG + "♚" + RESET
